b80043682f398409b149c7f5158059b5ddac2874
KARAN KUMAR GUPTA <107607829+karang152@users.noreply.github.com>
2023-02-21 21:54:08 +0530

    Update 540. Single Element in a Sorted Array.cpp

--- leetcode/540. Single Element in a Sorted Array.cpp
+++ leetcode/540. Single Element in a Sorted Array.cpp
@@ -1,1 +1,29 @@
+//https://leetcode.com/problems/single-element-in-a-sorted-array/submissions/902360234/
 
+
+
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (mid % 2 == 0) {
+                if (nums[mid] == nums[mid + 1]) {
+                    left = mid + 2;
+                } else {
+                    right = mid;
+                }
+            } else {
+                if (nums[mid] == nums[mid - 1]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return nums[left];
+    }
+};
