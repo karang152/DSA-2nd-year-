7c45e7128e817c1206240d1c3d6f6e61d4059145
KARAN KUMAR GUPTA <107607829+karang152@users.noreply.github.com>
2023-05-01 11:04:52 -0700

    Create 20. Valid Parentheses.cpp

---
+++ leetcode/20. Valid Parentheses.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/valid-parentheses/submissions/942780793/
+
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        stack<char> st;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '{' || s[i] == '(' || s[i] == '[') st.push(s[i]);
+            else if (!st.empty() && s[i] == '}' && st.top() == '{') st.pop();
+            else if (!st.empty() && s[i] == ')' && st.top() == '(') st.pop();
+            else if (!st.empty() && s[i] == ']' && st.top() == '[') st.pop();
+            else return false;
+        }
+        return st.empty();
+    }
+};
