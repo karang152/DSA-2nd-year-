bf1b55dbca9d7a32d27462f7743199f46bff828a
KARAN KUMAR GUPTA <107607829+karang152@users.noreply.github.com>
2023-05-05 18:11:21 +0530

    Create 48. Rotate Image.cpp

---
+++ leetcode/48. Rotate Image.cpp
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/rotate-image/submissions/944945617/
+
+
+
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        //transverse
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        //reverse
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
