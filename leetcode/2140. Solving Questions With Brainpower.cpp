8fb72a52ebe75da7024c6f85410ea57c9d102321
KARAN KUMAR GUPTA <107607829+karang152@users.noreply.github.com>
2023-05-12 21:46:49 +0530

    Create 2140. Solving Questions With Brainpower.cpp

---
+++ leetcode/2140. Solving Questions With Brainpower.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/solving-questions-with-brainpower/submissions/949089188/
+
+
+
+
+
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        vector<int64_t> dp(size(questions) + 1);
+        for (int i = size(dp) - 2; i >= 0; --i) {
+            dp[i] = max(dp[i + 1], questions[i][0] + (i + 1 + questions[i][1] < size(dp) ? dp[i + 1 + questions[i][1]] : static_cast<int64_t>(0)));
+        }
+        return dp[0];
+    }
+};
