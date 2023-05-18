5ae5fd670ce890eb2094cfb38eb9662e631e2392
KARAN KUMAR GUPTA <107607829+karang152@users.noreply.github.com>
2023-05-18 22:10:06 +0530

    Create 1557. Minimum Number of Vertices to Reach All Nodes .cpp

---
+++ leetcode/1557. Minimum Number of Vertices to Reach All Nodes .cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/submissions/952834778/
+
+
+
+
+
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<bool>visited(n);
+        for(auto edge:edges) visited[edge[1]]=1;
+        vector<int> ans;
+        for(int i=0; i<n; i++) {
+            if(visited[i]==0) ans.push_back(i);
+        }
+        return ans;
+    }
+};
